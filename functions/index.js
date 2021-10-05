c194bcccacfa8a50fe55a71817b74d9608efadff
Juan Yepes <yepes.j@hotmail.com>
2021-10-05 14:35:05 -0400

    deployed api with firebase

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,17 @@
 const functions = require("firebase-functions");
 const express = require('express')
-const { getAllProducts,getProductById } = require('./src/products')
+const cors = require('cors')
+const { getAllProducts,getProductById, createProduct, updateProduct, deleteProduct } = require('./src/products')
 
 const app = express()
+app.use(cors())
 
 app.get('/products/:productId', getProductById)
 app.get('/products', getAllProducts)
+app.post('/products', createProduct)
+app.patch('/products/:productId', updateProduct)
+app.delete('/products/:productId', deleteProduct)
+
 
 
 exports.app = functions.https.onRequest(app) // ((req,res) =>app(req, res))
--- functions/src/products.js
+++ functions/src/products.js
@@ -42,7 +42,45 @@ exports.getProductById = (req, res) => {
 }
 
 //createProduct
+exports.createProduct =(req, res) => {
+    // check that all required fields are present
+    if(!req.body.sku || !req.body.type || !req.body.price) {
+        res.status(401).send({message: 'invalid request'})
+        return
+    }
+
+    let newItem = {
+        sku: req.body.sku,
+        type: req.body.type,
+        price: Number(req.body.price.toFixed(2)),
+        graphic: req.body.graphic ? true : false,
+    }
+    if(req.body.sizes) newItem.sizes = req.body.sizes
+    if(req.body.color) newItem.color = req.body.color
+    if(req.body.brand) newItem.brand = req.body.brand
+    if(req.body.style) newItem.style = req.body.style
+
+    const db = connectDb()
+    db.collection('clothes').add(newItem)
+        .then(docRef => res.status(201).send({ id: docRef.id}))
+        .catch(err => res.status(500).send(err))
+}
 
 //updateProduct
+exports.updateProduct = (req, res) => {
+   const { productId } = req.params
+   const db = connectDb()
+   db.collection('clothes').doc(productId).update(req.body)
+    .then(() => res.status(202).send({message: "updated"}))
+    .catch(err => res.status(500).send(err))
+}
 
 // deleteProduct
+exports.deleteProduct = (req, res) => {
+    const { productId } = req.params
+    const db = connectDb()
+    db.collection('clothes').doc(productId).delete()
+        .then(() => res.status(202).send({message: 'deleted'}))
+        .catch(err => res.status(500).send(err))
+
+}
